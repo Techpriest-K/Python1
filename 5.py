911d87ef2229e0f1b3e1ae1816f1ee601c9fafe0
Techpriest-K <32102604+Techpriest-K@users.noreply.github.com>
2017-12-25 20:19:13 +0600

    Homework1
    Get started!

---
+++ 5.py
@@ -0,0 +1,27 @@
+for i in range(1500,2700):
+	if i%7 == 0 and i%5==0:
+			print(i)
+
+
+
+
+
+for x in range(-4,5):
+	if x ==-4:
+		print("*")
+	if x ==-3:
+		print("**")
+	if x ==-2:
+		print("***")
+	if x ==-1:
+		print("****")
+	if x ==0:
+		print("*****")
+	if x ==1:
+		print("****")
+	if x ==2:
+		print("***")
+	if x ==3:
+		print("**")
+	if x ==4:
+		print("*")
